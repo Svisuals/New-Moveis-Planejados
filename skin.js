3a0e4d4e3afdd9588ca6f17ff0f5fbd69351f139
Svisuals <feraso.svisuals@gmail.com>
2023-08-17 14:10:35 -0300

    UPDATE

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.13/18080
 // Filename: NEW Planejados.ggsk
-// Generated 2023-08-17T14:03:03
+// Generated 2023-08-17T14:08:07
 
 function pano2vrSkin(player,base) {
 	player.addVariable('Node1', 2, false);
